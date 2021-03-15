7119e831cd2731489aece90f9099e9fb6ac6bf59
Rob Jenks <robjenks@gmail.com>
2021-03-15 21:35:49 +0000

    Add pre shutdown hooks for monitor service

--- monitor-agent-awair/src/main/java/org/rj/homectl/monitor/awair/AwairMonitorAgent.java
+++ monitor-agent-awair/src/main/java/org/rj/homectl/monitor/awair/AwairMonitorAgent.java
@@ -8,22 +8,24 @@ import org.rj.homectl.status.events.StatusEventType;
 import org.rj.homectl.status.producer.awair.AwairStatusEventProducer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.client.RestTemplate;
 
-import java.time.OffsetDateTime;
-import java.util.Random;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 @SpringBootApplication
 @ComponentScan(basePackages = "org.rj")
 public class AwairMonitorAgent {
     private static final Logger log = LoggerFactory.getLogger(AwairMonitorAgent.class);
-
     private final AwairMonitorAgentService service;
     private AtomicBoolean active;
+
+    @Autowired
+    private ApplicationContext applicationContext;
 
     public static void main(String[] args) {
         SpringApplication.run(AwairMonitorAgent.class, args);
@@ -42,21 +44,23 @@ public class AwairMonitorAgent {
                 AwairMonitorAgent.class
         );
 
+        Runtime.getRuntime().addShutdownHook(new Thread(this::preShutdown));
+
         new Thread(() -> execute(producer)).start();
     }
 
     private void execute(AwairStatusEventProducer producer) {
         while (this.active.get()) {
-            log.info("Requesting status from Awair service");
-            final var status = getData(); // getTempStatus();
+            log.debug("Requesting status from Awair service");
+            final var status = getData();
 
-            log.info("Sending status ({})", Util.safeSerialize(status));
+            log.debug("Sending status ({})", Util.safeSerialize(status));
             producer.send(StatusEventType.Awair.getKey(), status);
             try {
                 Thread.sleep(1000L);
             }
             catch (Exception ex) {
-                log.error("*** Failed to suspend thread ({}) ***", ex.getMessage());
+                log.error("*** Failed to suspend monitor thread ({}) ***", ex.getMessage());
             }
         }
     }
@@ -75,26 +79,10 @@ public class AwairMonitorAgent {
     public void terminate(String reason) {
         log.info("Received termination signal ({})", reason);
         this.active.set(false);
+        SpringApplication.exit(applicationContext, () -> 0);
     }
 
-    private AwairStatusData getTempStatus() {
-        final var rand = new Random(System.currentTimeMillis());
-        final var status = new AwairStatusData();
-        status.setTimestamp(OffsetDateTime.now());
-        status.setAbsoluteHumidity(rand.nextFloat() * 100.0f);
-        status.setCO2(rand.nextInt(500));
-        status.setTemp(rand.nextFloat() * 10.0f + 18.0f);
-        status.setDewPoint(rand.nextFloat() * 100.0f);
-        status.setEstimatedCO2(rand.nextInt(500));
-        status.setEstimatedPM10(rand.nextInt(1000));
-        status.setHumidity(rand.nextFloat() * 100.0f);
-        status.setPM25(rand.nextInt(1000));
-        status.setVOC(rand.nextInt(1000));
-        status.setVOCBaseline((long)rand.nextInt(1000));
-        status.setVOCRawEthanol(rand.nextInt(1000));
-        status.setVOCRawH2(rand.nextInt(1000));
-        status.setScore(rand.nextInt(20) + 80);
-
-        return status;
+    private void preShutdown() {
+        log.info("JVM shutdown initiated, ending process");
     }
 }

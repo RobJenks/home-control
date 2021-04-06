8e11f74f2abe8be64df7eae498532c6f4df664d5
Rob Jenks <robjenks@gmail.com>
2021-04-06 12:32:19 +0100

    Add data size parameter for cache content

--- aggregation/src/main/java/org/rj/homectl/aggregation/controller/AggregationController.java
+++ aggregation/src/main/java/org/rj/homectl/aggregation/controller/AggregationController.java
@@ -6,10 +6,11 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Collections;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 public class AggregationController {
@@ -19,13 +20,19 @@ public class AggregationController {
         this.parent = parent;
     }
 
-    @GetMapping("/data")
-    public ResponseEntity<Map<String, Object>> data() {
-        final var data = parent.getCache().get().toList();
-        Collections.reverse(data);
+    @GetMapping("/updates")
+    public ResponseEntity<Map<String, Object>> updates(
+            @RequestParam(value = "count", required = false, defaultValue = "50") int count) {
+
+        if (count < 0) return new ResponseEntity<>(Map.of("error", "Invalid update count requested"), HttpStatus.BAD_REQUEST);
+
+        final var data = parent.getCache().get()
+                .reverseStream()
+                .limit(count)
+                .collect(Collectors.toList());
 
         final Map<String, Object> responseData = Map.of(
-                "count", parent.getCache().get().getCount(),
+                "count", data.size(),
                 "data", data
         );
 

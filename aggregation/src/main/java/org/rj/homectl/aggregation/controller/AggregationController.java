cc5bbb356ff22f97e665f5cf3ad65106d60046cb
Rob Jenks <robjenks@gmail.com>
2021-04-09 22:33:05 +0100

    Allow cross-origin requests to aggregation controller

--- aggregation/src/main/java/org/rj/homectl/aggregation/controller/AggregationController.java
+++ aggregation/src/main/java/org/rj/homectl/aggregation/controller/AggregationController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import java.util.Map;
 import java.util.stream.Collectors;
 
 @RestController
+@CrossOrigin
 public class AggregationController {
     private final Aggregation parent;
 

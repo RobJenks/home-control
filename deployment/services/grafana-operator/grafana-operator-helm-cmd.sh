65a99ef5dc7c846ae9c9b8674e9083bd6cf9d76d
Rob Jenks <robjenks@gmail.com>
2021-12-19 23:06:41 +0000

    Update helm config

--- deployment/services/grafana-operator/grafana-operator-helm-cmd.sh
+++ deployment/services/grafana-operator/grafana-operator-helm-cmd.sh
@@ -1,1 +1,2 @@
-sudo helm install home-control-grafana-operator grafana/grafana-agent-operator -f helm-values.yaml -n home-control --debug --kubeconfig=/etc/rancher/k3s/k3s.yaml --kube-context=home-control
+# Install helm from script here: https://helm.sh/docs/intro/install/
+sudo helm install home-control-grafana-operator grafana/grafana-agent-operator -f grafana-operator-helm-values.yaml -n home-control --debug --kubeconfig=/etc/rancher/k3s/k3s.yaml --kube-context=home-control
